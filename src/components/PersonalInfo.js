8b4d44d460cefa1e40ffd5ccdabdc466e109ca36
IkyaRaoChennadi <chennadiikyarao@gmail.com>
2024-08-25 22:28:11 +0530

    commit

--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -1,18 +1,18 @@
 // src/components/PersonalInfo.js
-import React from 'react';
-import { Box, Typography } from '@mui/material';
+import React from "react";
+import { Box, Typography } from "@mui/material";
 
 const PersonalInfo = ({ name, year, batch, position }) => {
   const getPositionStyle = () => {
     switch (position) {
-      case 'top':
-        return { textAlign: 'center', marginTop: '16px' };
-      case 'left':
-        return { position: 'absolute', top: '16px', left: '16px' };
-      case 'right':
-        return { position: 'absolute', top: '16px', right: '16px' };
-      case 'bottom':
-        return { textAlign: 'center', marginBottom: '16px' };
+      case "top":
+        return { textAlign: "center", marginTop: "16px" };
+      case "left":
+        return { position: "absolute", top: "16px", left: "16px" };
+      case "right":
+        return { position: "absolute", top: "16px", right: "16px" };
+      case "bottom":
+        return { textAlign: "center", marginBottom: "16px" };
       default:
         return {};
     }
@@ -20,7 +20,7 @@ const PersonalInfo = ({ name, year, batch, position }) => {
 
   return (
     <Box sx={getPositionStyle()}>
-      <Typography variant="h5">Binitha Basetty</Typography>
+      <Typography variant="h5">Ikya Rao Chennadi</Typography>
       <Typography variant="body1">{`Year: 3rd`}</Typography>
     </Box>
   );
